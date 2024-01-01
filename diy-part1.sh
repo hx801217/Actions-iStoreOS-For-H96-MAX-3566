37bdd69f4ed328a83dcb26e3c4d5c7a662642dad
hx801217 <34504398+hx801217@users.noreply.github.com>
2024-01-01 19:13:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-#===============================================
-# Description: DIY script
-# File name: diy-script.sh
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#===============================================
 
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d.%H")
-sed -i 's#2024010114/R${date_version}g' version
